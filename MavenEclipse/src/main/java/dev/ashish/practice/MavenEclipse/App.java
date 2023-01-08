63390c5a823ea1f17f77093703d327539b4a22ff
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-01-08 16:45:37 +0530

    Maven created by commandline and MavenEclipse created by Eclipse

---
+++ MavenEclipse/src/main/java/dev/ashish/practice/MavenEclipse/App.java
@@ -0,0 +1,13 @@
+package dev.ashish.practice.MavenEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
