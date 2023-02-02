1e9565183d8eac8cdf22f6c017ac0fef48fe02e6
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-02-02 19:15:28 +0530

    Unit Test section complete

---
+++ DAOPattern/src/test/java/dev/ashish/app/DAOPattern/ProfileTest.java
@@ -0,0 +1,16 @@
+package dev.ashish.app.DAOPattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProfileTest {
+	
+	@Test
+	public void testLoadDBConfig() {
+		var props=Profile.getProperties("db");
+		Assert.assertNotNull("Cannot load profile",props);
+		var db=props.getProperty("database");
+		Assert.assertEquals("dbName incorrect","peopletest",db);
+	}
+	
+}
